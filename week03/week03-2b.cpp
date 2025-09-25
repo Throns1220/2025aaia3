806444503b1956ce38cb98d0b1a827541c62094c
Throns1220 <bruce20121220@gmail.com>
2025-09-25 17:13:13 +0800

    week03

---
+++ week03/week03-2b.cpp
@@ -0,0 +1,18 @@
+/// week03-2b.cpp  璶糶2Ω (ぃ璶痷 nums[i] +1,-1,0)
+/// LeetCode 厩策璸礶材8肈 1822. Sign of the Product of an Array
+/// р皚癬ㄓ琌タ计璽计临琌 0
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int ans = 1; ///  0 ヴ计常穦跑Θ0Τ1ぃ跑
+        for(int i=0;i<nums.size();i++) { /// Τ碭计癹伴禲碭Ω
+            if(nums[i]>0) ans*= 1;
+            if(nums[i]<0) ans*= -1;
+            if(nums[i]==0) ans*= 0;
+            /// ans *= nums[i]; /// –Ωр nums[i] 秈 ans 柑
+        } /// 计禫禫1000计碞
+        if(ans>0) return 1;
+        if(ans<0) return -1;
+        return 0;
+    }
+};
