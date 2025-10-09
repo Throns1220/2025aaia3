48bdf4842ad271f56e00ce6c99482f221582f4a2
Throns1220 <bruce20121220@gmail.com>
2025-10-09 17:07:35 +0800

    week05

---
+++ week05/week05-3a.cpp
@@ -0,0 +1,10 @@
+/// week05-3a.cpp
+/// CPE 的第2題 UVA 483 倒過來
+#include <iostream>
+using namespace std;
+int main(){
+    string line; /// 一行字的字串
+    while(getline(cin, line)){ /// 讀進來
+        cout << line << endl; /// Part 2: Output
+    }
+}
---
+++ week05/week05-4.cpp
@@ -0,0 +1,12 @@
+/// week05-4.cpp
+/// LeetCode 學習計畫 Bulit-In Functions 第2題 709. To Lower Case
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (int i=0;i<s.length();i++){
+            s[i] = tolower(s[i]);
+        } /// 每個字母, 都變成小寫 (完整版要 #include <ctype.h>)
+        /// 或是用 #include <cctype> 兩個是同一個檔案
+        return s; /// 答案送出去
+    }
+};
