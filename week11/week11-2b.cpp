e947c2a6f065b417430f818bed9193e9ffc24886
Throns1220 <bruce20121220@gmail.com>
2025-11-20 16:44:42 +0800

    week10

---
+++ week11/week11-2b.cpp
@@ -0,0 +1,17 @@
+/// week11-2b.cpp
+/// SOIT107_ADVANCE_009_C_C++
+#include <iostream>
+using namespace std;
+int max_digit(int n){
+	int ans=n%10;
+	while(n>0) { // / Bo-Pi Fa
+		if(n%10>ans) ans=n%10;
+		n=n/10;
+	}
+	return ans;
+}
+int main(){
+  int n;cin>>n;
+  cout<<"["<<max_digit(n)<<"]";
+  return 0;
+}
